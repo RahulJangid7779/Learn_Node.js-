90448725a09797b3f21d94ca8531bdbb3d5bf1da
Rahul Jangid <136984487+RahulJangid7779@users.noreply.github.com>
2024-10-24 21:12:24 +0530

    Add files via upload

--- Learn_Node.js-/database.js
+++ Learn_Node.js-/database.js
@@ -1,8 +1,9 @@
 // Create a mongodb website
 // create a free mb cluster
 // create server
+// 7lcUjYPmexXHc6Tq
 // create the connection string
-
+// connect to mongodb connect
+require("mongodb");
 const URL =
-  "mongodb+srv://rahuljangid:<db_password>@rahuljangid.ygog2.mongodb.net/";
-// Download mongodb compass
+  "mongodb+srv://rahul6898sharma:7lcUjYPmexXHc6Tq@rahul1data.89iyr.mongodb.net/";
